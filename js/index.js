831ac11946678c44814dacd62541fcdef0b0ef17
ryo <ryokomori9696@gmail.com>
2021-07-11 23:56:50 +0900

    modal update

--- js/index.js
+++ js/index.js
@@ -98,6 +98,11 @@ const close = document.getElementById("close");
 const modal = document.getElementById("modal");
 const mask = document.getElementById("mask");
 
+const open_2 = document.getElementById("open_2");
+const close_2 = document.getElementById("close_2");
+const modal_2 = document.getElementById("modal_2");
+const mask_2 = document.getElementById("mask_2");
+
 open.addEventListener("click", () => {
   modal.classList.remove("hidden");
   mask.classList.remove("hidden");
@@ -109,4 +114,17 @@ close.addEventListener("click", () => {
 mask.addEventListener("click", () => {
   modal.classList.add("hidden");
   mask.classList.add("hidden");
+});
+
+open_2.addEventListener("click", () => {
+  modal_2.classList.remove("hidden_2");
+  mask_2.classList.remove("hidden_2");
+});
+close_2.addEventListener("click", () => {
+  modal_2.classList.add("hidden_2");
+  mask_2.classList.add("hidden_2");
+});
+mask_2.addEventListener("click", () => {
+  modal_2.classList.add("hidden_2");
+  mask_2.classList.add("hidden_2");
 });
